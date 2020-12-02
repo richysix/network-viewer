97258903ca3ad8c4282cdeafe8e569b8b00fc948
Richard White <rjw26@cam.ac.uk>
2020-12-02 16:46:48 +0000

    Change to using indices for connecting nodes and edges
    Added field type conversion functions for input data

--- js/graph-svg.js
+++ js/graph-svg.js
@@ -1,19 +1,53 @@
-const testing = true;
+const testing = false;
 const debug = true;
 
 var width = 960;
-var height = 600;
+var height = 960;
 
 if (testing) {
     nodes_data_file = "http://localhost:8010/data/nodes-test.csv";
     edges_data_file = "http://localhost:8010/data/edges-test.csv";
+} else {
+    nodes_data_file = "http://localhost:8010/data/nodes.csv";
+    edges_data_file = "http://localhost:8010/data/edges.csv";
 }
+convert_node_data_fields = function(d) {
+    return {
+        node_idx: parseInt(d.node_idx),
+        id: parseInt(d.id),
+        gene_id: d.gene_id,
+        gene_name: d.gene_name,
+        cluster_id: parseInt(d.cluster_id)
+    };
+}
+convert_edge_data_fields = function(d) {
+    return {
+        edge_idx: parseInt(d.edge_idx),
+        source: parseInt(d.source),
+        target: parseInt(d.target),
+        weight: parseFloat(d.weight)
+    };
+}
+
 Promise.all([
-    d3.csv(nodes_data_file),
-    d3.csv(edges_data_file)
+    d3.csv(nodes_data_file, convert_node_data_fields),
+    d3.csv(edges_data_file, convert_edge_data_fields)
 ]).then(function(data) {
     draw_network( data[0], data[1] );
 });
+
+//var nodes = [
+//  {"id": "Alice"},
+//  {"id": "Bob"},
+//  {"id": "Carol"}
+//];
+//
+//var links = [
+//  {"source": 0, "target": 1}, // Alice → Bob
+//  {"source": 1, "target": 2} // Bob → Carol
+//];
+
+//draw_network(nodes, links);
 
 function draw_network(node_data, edge_data) {
     if (debug) {
@@ -33,8 +67,7 @@ function draw_network(node_data, edge_data) {
     //set up the simulation 
     //nodes only for now 
     var simulation = d3.forceSimulation()
-                  .nodes(node_data);
-    
+                        .nodes(node_data);
     //add forces
     //we're going to add a charge to each node 
     //also going to add a centering force
@@ -44,10 +77,9 @@ function draw_network(node_data, edge_data) {
     
     //Create the link force 
     //We need the id accessor to use named sources and targets 
-    var link_force =  d3.forceLink(edge_data)
-                            .id(function(d) { return d.id; });
+    var link_force =  d3.forceLink(edge_data).id(function id(d) {return d.index;});
     
-    simulation.force("links",link_force);
+    simulation.force("links", link_force);
     
     // Define the div for the tooltip
     var tooltip_div = d3.select("body").append("div")	
@@ -87,20 +119,20 @@ function draw_network(node_data, edge_data) {
     // Colours
     const scale = d3.scaleOrdinal(d3.schemeTableau10);
     function colour_cluster(d) {
-        cluster_idx = d.cluster;
+        cluster_idx = d.cluster_id;
         if (debug) {
             //console.log(d);
             console.log(cluster_idx);
         }
         if (d3.select(this).classed("selected")) {
             svg.selectAll("circle")
-                .filter(function(d){ return d.cluster ==  cluster_idx ? this : null; })
+                .filter(function(d){ return d.cluster_id ==  cluster_idx ? this : null; })
                 .attr("fill", node_unselected_color)
                 .each(function(){ this.classList.toggle("selected"); });
         } else {
             svg.selectAll("circle")
-                .filter(function(d){ return d.cluster ==  cluster_idx ? this : null; })
-                .attr("fill", scale(d.cluster))
+                .filter(function(d){ return d.cluster_id ==  cluster_idx ? this : null; })
+                .attr("fill", scale(d.cluster_id))
                 .each(function(){ this.classList.toggle("selected"); });
         }
     }
@@ -113,11 +145,11 @@ function draw_network(node_data, edge_data) {
         });
     
     function node_info(d) {
-        box_width = 40 + d.name.length*6 + 10;
+        box_width = 40 + d.gene_name.length*6 + 10;
         if (debug) {
             console.log(box_width);
         }
-        tooltip_div.html("name = " + d.name + "<br/>"  + "cluster = " + d.cluster)	
+        tooltip_div.html("name = " + d.gene_name + "<br/>"  + "cluster = " + d.cluster_id)	
                     .style("left", (d3.event.pageX) + "px")		
                     .style("top", (d3.event.pageY - 28) + "px")
                     .style("width", box_width);
