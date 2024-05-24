2bec02198f8019c37766e67a373ea4a93f1f4799
Richard White <richard.white@qmul.ac.uk>
2024-05-24 15:03:57 +0100

    Add ability to drag nodes and colour clusters
    Shift-click now colours the cluster of the selected node
    click-drag drags the node

--- js/graph-svg.js
+++ js/graph-svg.js
@@ -1,20 +1,21 @@
 const testing = false;
 const mini = true;
 const debug = true;
-const width = 960, height = 960;
+const width = 480, height = 480;
+const charge_force_strength = -10;
 
 // set height and width of svg element
-document.querySelector('#graph_svg').setAttribute("height", height);
-document.querySelector('#graph_svg').setAttribute("width", width);
+document.querySelector('#graph-svg').setAttribute("height", height);
+document.querySelector('#graph-svg').setAttribute("width", width);
 
 /// define readers
 let reader1 = new FileReader();
 let reader2 = new FileReader();
 
 function load_nodes_file() {
-    var file = document.querySelector('#nodes_file').files[0];
+    var file = document.querySelector('#nodes-file').files[0];
     if (debug) {
-        console.log(document.querySelector('#nodes_file').files);
+        console.log(document.querySelector('#nodes-file').files);
         console.log(file);
     }
     reader1.addEventListener("load", draw_network, false);
@@ -24,7 +25,7 @@ function load_nodes_file() {
 }
 
 function load_edges_file() {
-    var file = document.querySelector('#edges_file').files[0];
+    var file = document.querySelector('#edges-file').files[0];
     reader2.addEventListener("load", draw_network, false);
     if (file) {
       reader2.readAsText(file);
@@ -32,28 +33,44 @@ function load_edges_file() {
 }
 
 // functions to set the types of the incoming data
-convert_node_data_fields = function(d) {
+function convert_node_data_fields(d) {
     return {
         node_idx: parseInt(d.node_idx),
-        id: parseInt(d.id),
+//        id: parseInt(d.id),
         gene_id: d.gene_id,
         gene_name: d.gene_name,
         cluster_id: parseInt(d.cluster_id)
     };
-};
-convert_edge_data_fields = function(d) {
+}
+
+function convert_edge_data_fields(d) {
     return {
         edge_idx: parseInt(d.edge_idx),
         source: parseInt(d.source),
         target: parseInt(d.target),
         weight: parseFloat(d.weight)
     };
-};
+}
+
+function reset_network() {
+    d3.select("#graph-svg").selectAll("*").remove();
+    //document.getElementById('nodes-file').value = '';
+    //document.getElementById('edges-file').value = '';
+    document.getElementById('filesform').reset();
+    reader1.result = undefined;
+    reader2.result = undefined;
+    draw_network();
+}
+d3.select('#reset').on("click", reset_network);
 
 function draw_network() {
+    // reset the network
+    d3.select("#graph-svg").selectAll("*").remove();
+
     if (!reader1.result || !reader2.result) {
         return;
     }
+    
     let node_data = d3.csvParse(reader1.result, convert_node_data_fields);
     let edge_data = d3.csvParse(reader2.result, convert_edge_data_fields);
 
@@ -65,26 +82,22 @@ function draw_network() {
     const svg = d3.select("svg")
         .attr("viewBox", [0, 0, width, height]);
     
-    // Add zoom
-    const zoom = d3.zoom()
-        .extent([[0, 0], [width, height]])
-        .scaleExtent([1, 40])
-        .on("zoom", zoomed);    
-
     //set up the simulation 
     //nodes only for now 
-    var simulation = d3.forceSimulation()
+    const simulation = d3.forceSimulation()
                         .nodes(node_data);
     //add forces
     //we're going to add a charge to each node 
     //also going to add a centering force
     simulation
         .force("charge_force", d3.forceManyBody())
+        //.force("charge_force", d3.forceManyBody().strength(charge_force_strength))
         .force("center_force", d3.forceCenter(width / 2, height / 2));
     
     //Create the link force 
     //We need the id accessor to use named sources and targets 
     var link_force =  d3.forceLink(edge_data).id(function id(d) {return d.node_idx;});
+    //link_force.strength(function weight(d) {return d.weight / 10;});
     
     simulation.force("links", link_force);
     
@@ -94,8 +107,8 @@ function draw_network() {
         .style("opacity", 0);
     
     //draw lines for the links 
-    var links = svg.append("g")
-          .attr("class", "links")
+    const links = svg.append("g")
+        .attr("class", "links")
         .selectAll("line")
         .data(edge_data)
         .enter().append("line")
@@ -103,87 +116,92 @@ function draw_network() {
     
     //draw circles for the nodes
     const node_unselected_color = "#eeeeee";
-    var nodes = svg.append("g")
-            .attr("class", "nodes")
-            .selectAll("circle")
-            .data(node_data)
-            .enter()
-            .append("circle")
-            .attr("r", 10)
-            .attr("fill", node_unselected_color)
-            .attr("stroke", "#333333")
-            .attr("cx", width / 2)
-            .attr("cy", height / 2);
-            
-    const g = svg.selectAll("g");
-
-    svg.call(zoom);
-
-    simulation.on("tick", tickActions );
+    const nodes = svg.append("g")
+        .attr("class", "nodes")
+        .attr("cursor", "grab")
+        .selectAll("circle")
+        .data(node_data)
+        .join("circle")
+        .attr("r", 10)
+        .attr("fill", node_unselected_color)
+        .attr("stroke", "#333333")
+        .attr("cx", width / 2)
+        .attr("cy", height / 2);
     
-    nodes.on("click", colour_cluster);
-    
+    nodes.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended))
+        .on("click", colour_cluster);
+
+    function dragstarted(event, d) {
+        if (debug) {
+            console.log(d)
+            console.log(this)
+            console.log(event)
+        }
+        d3.select(this)
+            .raise()
+            .attr("fill", "#000000");
+        nodes.attr("cursor", "grabbing");
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+    }
+    function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+    }
+    function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+        if (d3.select(this).classed("selected")) {
+            d3.select(this)
+                .attr("fill", scale(d.cluster_id));
+        } else {
+            d3.select(this)
+                .attr("fill", node_unselected_color);
+        }
+        nodes.attr("cursor", "grab");
+    }
+
     // Colours
     const scale = d3.scaleOrdinal(d3.schemeTableau10);
-    function colour_cluster(d) {
-        cluster_idx = d.cluster_id;
-        if (debug) {
-            //console.log(d);
-            console.log(cluster_idx);
-        }
-        if (d3.select(this).classed("selected")) {
-            svg.selectAll("circle")
+    function colour_cluster(event, d) {
+        if (event.shiftKey) {
+            cluster_idx = d.cluster_id;
+            if (debug) {
+                //console.log(d);
+                console.log(cluster_idx);
+            }
+            if (d3.select(this).classed("selected")) {
+                svg.selectAll("circle")
                 .filter(function(d){ return d.cluster_id ==  cluster_idx ? this : null; })
                 .attr("fill", node_unselected_color)
                 .each(function(){ this.classList.toggle("selected"); });
-        } else {
-            svg.selectAll("circle")
+            } else {
+                svg.selectAll("circle")
                 .filter(function(d){ return d.cluster_id ==  cluster_idx ? this : null; })
                 .attr("fill", scale(d.cluster_id))
                 .each(function(){ this.classList.toggle("selected"); });
+            }
         }
+        if (event.defaultPrevented) return; // dragged
     }
-    
-    nodes.on("mouseover", node_info)
-        .on("mouseout", function(){
-            tooltip_div.transition()		
-                .duration(500)		
-                .style("opacity", 0);
-        });
-    
-    function node_info(d) {
-        box_width = 40 + d.gene_name.length*6 + 10;
-        if (debug) {
-            console.log(box_width);
-        }
-        tooltip_div.html("name = " + d.gene_name + "<br/>"  + "cluster = " + d.cluster_id)	
-                    .style("left", (d3.event.pageX) + "px")		
-                    .style("top", (d3.event.pageY - 28) + "px")
-                    .style("width", box_width);
-        tooltip_div.transition()
-            .duration(200)
-            .style("opacity", '.9');
+
+    // Add zoom
+    const g = svg.selectAll("g");
+    function zoomed({transform}) {
+        g.attr("transform", transform);
     }
-    
-    function zoomed() {
-        if (debug) {
-            console.log(d3.event);
-        }
-        g.attr("transform", d3.event.transform);
-    }
-    
-    // Add functions for Zoom In, Out and Reset
-    d3.select("#zoom-in").on("click", function(){ svg.transition().call(zoom.scaleBy, 2); });
-    d3.select("#zoom-out").on("click", function(){ svg.transition().call(zoom.scaleBy, 0.5); });
-    d3.select("#reset").on("click", reset );
-    function reset() {
-        svg.transition().duration(750).call(
-          zoom.transform,
-          d3.zoomIdentity,
-          d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
-        );
-    }
-    
+    const zoom = d3.zoom()
+        .extent([[0, 0], [width, height]])
+        .scaleExtent([1, 40])
+        .on("zoom", zoomed);    
+
+    svg.call(zoom);
+
     function tickActions() {
         //update circle positions to reflect node updates on each tick of the simulation 
         nodes
@@ -199,5 +217,46 @@ function draw_network() {
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
     }
+    simulation.on("tick", tickActions );
+
+    nodes.on("mouseover", node_info)
+        .on("mouseout", function(){
+            tooltip_div.transition()		
+                .duration(500)		
+                .style("opacity", 0);
+        });
+    
+    function node_info(event) {
+        if (debug) {
+            console.log(event)
+            console.log(event.target.__data__)
+        }
+        d = event.target.__data__
+        box_width = 40 + d.gene_name.length*6 + 10;
+        if (debug) {
+            console.log(box_width);
+        }
+        tooltip_div.html("name = " + d.gene_name + "<br/>"  + "cluster = " + d.cluster_id)	
+                    .style("left", (event.pageX) + "px")		
+                    .style("top", (event.pageY - 28) + "px")
+                    .style("width", box_width);
+        tooltip_div.transition()
+            .duration(200)
+            .style("opacity", '.9');
+    }
+    
+    // Add functions for Zoom In, Out and Reset
+    d3.select("#zoom-in").on("click", function(){ svg.transition().call(zoom.scaleBy, 2); });
+    d3.select("#zoom-out").on("click", function(){ svg.transition().call(zoom.scaleBy, 0.5); });
+    d3.select("#reset-zoom").on("click", reset);
+    function reset() {
+        svg.transition().duration(750).call(
+          zoom.transform,
+          d3.zoomIdentity,
+          d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
+        );
+    }
+    
+    invalidation.then(() => simulation.stop());
 }
 
